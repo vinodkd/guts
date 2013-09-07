7f6993f9cbbc5aca15ba9b2b634e29ef45f85494
vinodkd <vinod@vinodkd.org>
2013-09-07 15:40:27 -0500

    Moved the converter tooling to tools dir. Changed toHTML.js to work with the new dir structure

--- book/tohtml.sh
+++ book/tohtml.sh
@@ -1,2 +1,12 @@
+#! /bin/bash
+
 #tohtml.sh
-node toHTML.js $1 > $2
+echo tohtml usage: from guts/book, invoke:
+echo     tohtml.sh xxx.md
+echo  This will convert src/xxx.md to html and put it in out/xxx.html
+
+cd tools
+filename=${1##*/}
+fnroot=${filename%.*}
+node toHTML.js ../src/$1 > ../out/$fnroot.html
+cd ..
