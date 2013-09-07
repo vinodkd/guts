cbc016bb06850321e007ff9f4ebafc12095d71d5
vinodkd <vinod@vinodkd.org>
2013-09-07 16:01:34 -0500

    Completed tooling work on tohtml.sh so that it takes the output of marked and wraps a html header and footer to it. The primary purpose of the html wrapper is to include a reference to guts.css so that the output can be styled.
    Still to do: actually move the md files to src, and add the output htmls to source control.

--- book/tohtml.sh
+++ book/tohtml.sh
@@ -1,12 +1,20 @@
 #! /bin/bash
 
 #tohtml.sh
-echo tohtml usage: from guts/book, invoke:
-echo     tohtml.sh xxx.md
-echo  This will convert src/xxx.md to html and put it in out/xxx.html
+# tohtml usage: from guts/book, invoke:
+#     tohtml.sh xxx.md
+#  This will convert src/xxx.md to html and put it in out/xxx.html
 
 cd tools
 filename=${1##*/}
 fnroot=${filename%.*}
-node toHTML.js ../src/$1 > ../out/$fnroot.html
+outFile=../out/$fnroot.html
+
+echo -n Converting src/$1 to $outFile ...
+
+cat ../out/top.html > $outFile
+node toHTML.js ../src/$1 >> $outFile
+cat ../out/bot.html >> $outFile
+
+echo done.
 cd ..
